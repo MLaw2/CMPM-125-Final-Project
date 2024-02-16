56ebb75043e683fbfc098e5f0524e7d48ac51870
Michael <l4wmich43l@gmail.com>
2024-02-15 23:16:40 -0800

    Added basic physics-based movement

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Vector3 move_vector;
+    private Rigidbody rbody;
+    [SerializeField] private float move_speed;
+    [SerializeField] private float jump_strength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move_vector = new UnityEngine.Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        UnityEngine.Vector3.Normalize(move_vector);
+        rbody.AddForce(move_vector * move_speed);
+        if (Input.GetKeyDown("space")) {
+            rbody.AddForce(Vector3.up * jump_strength, ForceMode.Impulse);
+        }
+
+    }
+}
