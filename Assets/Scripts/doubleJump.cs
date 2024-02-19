5bd7b561b193b2c98cf612d9e198c01b997798d1
htran34 <81715973+htran34@users.noreply.github.com>
2024-02-19 12:21:07 -0800

    Adding double jump script

---
+++ Assets/Scripts/doubleJump.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doubleJump : MonoBehaviour
+{
+    Rigidbody playerRigidbody;
+    public float jumpHeight = 10f;
+    public bool isGrounded;
+
+    public int maxJumpCount = 2;
+    private int jumpsRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        jumpsRemaining = maxJumpCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded || jumpsRemaining > 0)
+            {
+                Jump();
+            }
+        }
+    }
+
+    void Jump()
+    {
+        playerRigidbody.velocity = new Vector3(playerRigidbody.velocity.x, 0f, playerRigidbody.velocity.z);
+        playerRigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        jumpsRemaining--;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+            jumpsRemaining = maxJumpCount;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = false;
+        }
+    }
+}
