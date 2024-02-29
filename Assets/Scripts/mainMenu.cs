0ec50ee460146e368516da9ba3b8752db6388879
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2024-02-29 15:22:14 -0800

    menus added and mainmenu button added to pause screen

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class mainMenu : MonoBehaviour
+{
+    public string FirstLevel;
+    public string Credits;
+    public string Options;
+    public string MainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void start()
+    {
+
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(FirstLevel);
+    }
+
+    public void options()
+    {
+        SceneManager.LoadScene(Options);
+    }
+    public void credits()
+    {
+        SceneManager.LoadScene(Credits);
+    }
+    public void menu()
+    {
+        SceneManager.LoadScene(MainMenu);
+    }
+}
+
