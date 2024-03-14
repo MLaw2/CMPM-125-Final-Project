fe9299cf4718d89655b66ce03aae212db39ce975
Michael <l4wmich43l@gmail.com>
2024-03-13 20:06:04 -0700

    Fixed pause menu

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -44,12 +44,13 @@ public class UIController : MonoBehaviour
         if(pauseScreen.activeSelf)
         {
             Cursor.lockState = CursorLockMode.None;
-
+            Cursor.visible = true;
             Time.timeScale = 0f;
         }
         else
         {
             Cursor.lockState= CursorLockMode.Locked;
+            Cursor.visible = false;
             Time.timeScale = 1f;
         }
     }
